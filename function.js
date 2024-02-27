bdce0e8eebbbc3877ac9f3210eacca570ada27a0
Vinícius Schaidt <schaidt@hotmail.com>
2024-02-27 16:26:00 -0300

    Initial commit

---
+++ function.js
@@ -0,0 +1,7 @@
+const progresso = document.querySelector(".barradiv");
+const Input = document.querySelector("input");
+
+const alterarProgresso = () => {
+    progresso.setAttribute("style", "width: 60%");
+}
+
